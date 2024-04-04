c0bdce04a546edaa74d1358ce5cdf9cc1f64d0ed
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-04 14:25:56 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const sendFile = async (i tem, ctx) => {
       await ctx.replyWithDocument(item);
     } catch (e) {
       // Sending the first error message
-      await ctx.replyWithMarkdown(`⚠️ ${e.message} \n Try Downloading the below link manually` );
+      await ctx.replyWithMarkdown(`⚠️ ${e.message} \n Try Downloading the below link manually,\n OR \n Use Our [Url Uploader Bot](https://t.me/UrlUploaderio_bot) to upload remotely to telegram` );
       
       // Sending the second message with the link
       await ctx.replyWithMarkdown(
